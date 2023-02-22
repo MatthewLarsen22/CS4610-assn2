1e3ae74a82209eb3cffafbd2a9f8c9b08dd3c594
Matthew Larsen <matt4.0larsen@gmail.com>
2023-02-22 10:20:03 -0700

    Cleaning out db and starting fresh

--- prisma/migrations/20230222171908_cleaning_out_db_and_starting_fresh/migration.sql
+++ prisma/migrations/20230222171908_cleaning_out_db_and_starting_fresh/migration.sql
@@ -1,4 +1,27 @@
 -- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Reptile" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" INTEGER NOT NULL,
+    "species" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "sex" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Reptile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
 CREATE TABLE "Feeding" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "reptileId" INTEGER NOT NULL,
@@ -40,3 +63,6 @@ CREATE TABLE "Schedule" (
     CONSTRAINT "Schedule_reptileId_fkey" FOREIGN KEY ("reptileId") REFERENCES "Reptile" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "Schedule_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
